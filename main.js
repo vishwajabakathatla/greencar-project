4a334c7b2eef9fc063e97976ecd8afad5e70ee0d
vishwajabakathatla <88708444+vishwajabakathatla@users.noreply.github.com>
2021-10-04 12:10:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,25 +1,29 @@
-//Create a reference for canvas 
-
-//Give specific height and width to the car image
-
+canvas= document.getElementById("myCanvas");
+ctx=canvas.getContext("2d");
+greencar_width=75;
+greencar_height=100;
+greencar_x = 5;
+greencar_y = 255;
 background_image = "parkingLot.jpg";
 greencar_image = "car2.png";
-
-//Set initial position for a car image.
+greencar_speed = 10;
 
 function add() {
-	//upload car, and background images on the canvas.
+  backgroundimgtag = new Image();
+  backgroundimgtag.onload = background_image;
+  backgroundimgtag.src = background_image;
+
+ carimgtag = new Image();
+ carimgtag.onload = greencar_image;
+ carimgtag.src = greencar_image;
 }
 
 function uploadBackground() {
-	//Define function ‘uploadBackground’
-
+	ctx.drawImage(backgroundimgtag,0,0,canvas.width,canvas.height);
 }
 
 function uploadgreencar() {
-	//Define function ‘uploadgreencar’.
-
-	
+	ctx.drawImage(carimgtag,greencar_x,greencar_y,greencar_width,greencar_height);
 }
 
 
@@ -56,22 +60,30 @@ function my_keydown(e)
 		
 }
 
-function up()
-{
-	//Define function to move the car upward
+function up(){
+ ctx.clearRect(greencar_x, greencar_y, canvas.width, canvas.height);
+ uploadBackground();
+ greencar_y -= greencar_speed;
+ uploadgreencar();
 }
 
-function down()
-{
-	//Define function to move the car downward
+function down(){
+ ctx.clearRect(greencar_x, greencar_y, canvas.width, canvas.height);
+ uploadBackground();
+ greencar_y += greencar_speed;
+ uploadgreencar();
 }
 
-function left()
-{
-	//Define function to move the car left side
+function left(){
+ ctx.clearRect(greencar_x, greencar_y, canvas.width, canvas.height);
+ uploadBackground();
+ greencar_x -= greencar_speed;
+ uploadgreencar();
 }
 
-function right()
-{
-	//Define function to move the car right side
+function right(){
+ ctx.clearRect(greencar_x, greencar_y, canvas.width, canvas.height);
+ uploadBackground();
+ greencar_x += greencar_speed;
+ uploadgreencar();
 }
